b2284fa4e3bf916e711776a4919a3def72f2685d
Ritesh Koushik <riteshkoushik39@gmail.com>
2023-12-03 03:57:03 +0530

    remove: 01

--- 01-intro/src/main.js
+++
@@ -1,8 +0,0 @@
-import './assets/main.css'
-
-import { createApp } from 'vue'
-// The createApp API allows multiple Vue applications to co-exist on the 
-// same page, each with its own scope for configuration and global assets
-import App from './App.vue'
-
-createApp(App).mount('#app')
