d1eb300e1cdf605a7b2996fcf620f55506880a48
Ritesh Koushik <riteshkoushik39@gmail.com>
2023-12-02 12:06:09 +0530

    recreate: SPA with Toolchain

---
+++ vue-start-spa/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <p>Simple Text</p>
+</template>
+
+<script>
+
+import Navbar from './components/Navbar.vue';
+import PageViewer from './components/PageViewer.vue';
+
+export default {
+  components: {
+    Navbar,
+    PageViewer
+  },
+  data(){
+    return {
+      activePage: 0,
+      pages: [
+        {
+          link: {text: "Home", url: "index.html"},
+          pageTitle: "Home Page",
+          content: "This is the home page content"
+        },
+        {
+          link: {text: "About", url: "about.html"},
+          pageTitle: "Home Page",
+          content: "This is the home page content"
+        },
+        {
+          link: {text: "Contact", url: "contact.html"},
+          pageTitle: "Contact Page",
+          content: "This is the contact page content"
+        },
+
+
+
+      ]
+    }
+  }
+}
+</script>
