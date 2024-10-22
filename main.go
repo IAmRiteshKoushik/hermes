69ad1d3fcd2e96e3aa968e2b6252cbac86e9a85c
IAmRiteshKoushik <riteshkoushik39@gmail.com>
2024-10-23 00:53:36 +0530

    Init main program

--- main.go
+++ main.go
@@ -1,1 +1,19 @@
 package main
+
+import (
+	"fmt"
+	"os"
+	"os/user"
+
+	"github.com/IAmRiteshKoushik/hermes/repl"
+)
+
+func main() {
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Hello %s! This is the RK programming language!\n", user.Username)
+	fmt.Printf("Feel free to type in commands\n")
+	repl.Start(os.Stdin, os.Stdout)
+}
